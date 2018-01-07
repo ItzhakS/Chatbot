2740c660eccf56f16ea3b55f67d2ff34384220e0
Itzchuck <ieshachnovitz@gmail.com>
2018-01-07 11:21:12 +0200

    Set up MongoDb, Mongoose

--- server.js
+++ server.js
@@ -3,10 +3,13 @@ var bodyParser = require("body-parser")
 var app = express();
 var http = require("http").Server(app)
 var io = require("socket.io")(http)
+var mongoose = require('mongoose')
 
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+var dbUrl = 'mongodb://ChatbotAdmin:ChatbotAdmin@ds239177.mlab.com:39177/learning_node'
 
 var messages = [
     {name: "Jon", message: "Yo"},
@@ -19,11 +22,16 @@ app.get("/messages", (req , res) => {
 
 app.post("/messages", (req , res) => {
     messages.push(req.body)
+    io.emit('message', req.body)
     res.sendStatus(200)
 })
 
 io.on("connection", (socket) => {
-    
+
+})
+
+mongoose.connect(dbUrl,  (err) => {
+
 })
 
 var server = http.listen(3000, () => {
