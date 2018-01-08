8e376c37baccba6599ba90208153cf4f7cc8d1cd
Itzchuck <ieshachnovitz@gmail.com>
2018-01-08 11:42:36 +0200

    Catch and censor silly words

--- server.js
+++ server.js
@@ -1,3 +1,5 @@
+import { retry } from "../../../AppData/Local/Microsoft/TypeScript/2.6/node_modules/@types/async";
+
 var express = require("express");
 var bodyParser = require("body-parser")
 var app = express();
@@ -8,6 +10,8 @@ var mongoose = require('mongoose')
 app.use(express.static(__dirname))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({extended: false}))
+
+mongoose.Promise = Promise
 
 var dbUrl = 'mongodb://ChatbotAdmin:ChatbotAdmin@ds239177.mlab.com:39177/learning_node'
 
@@ -27,17 +31,28 @@ app.get("/messages", (req , res) => {
     })
 })
 
-app.post("/messages", (req , res) => {
+app.post("/messages", (req, res) => {
     var message = new Message(req.body)
 
-    message.save((err) =>{
-        if(err)
-            sendStatus(500)
+    message.save()
+        .then(() => {
+            console.log('saved')
+            return Message.findOne({ message: 'silly' })
+        })
+        .then(censored => {
+            if (censored) {
+                console.log('Found censored word')
+                return Message.remove({ _id: censored.id })
+            }
+            io.emit('message', req.body)
+            res.sendStatus(200)
+        })
+        .catch((err) => {
+            res.sendStatus(500)
+            return console.error(err)
+        })
+    // messages.push(req.body)
 
-        messages.push(req.body)
-        io.emit('message', req.body)
-        res.sendStatus(200)
-    })
 })
 
 io.on("connection", (socket) => {
@@ -51,5 +66,4 @@ mongoose.connect(dbUrl,  (err) => {
 
 var server = http.listen(3000, () => {
     console.log("Server is listening", server.address().port)
-}
-)
+})
